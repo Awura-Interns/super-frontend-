1f9cb1f7ba2747f384292c015b410acb15551a0a
Abigiya Terefe <abigiya1122@gmail.com>
2022-07-26 01:49:49 -0500

    modified the index page

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -9,83 +9,8 @@ const Add =()=>{
 
         <body>
 
-        <div class="sidebar">
-          <div class="logo-details">
-            <i class='bx bxl-c-plus-plus'></i>
-            <span class="logo_name">super link</span>
-          </div>
-          <ul class="nav-links">
-            <li>
-              <a href="/index.html" >
-                <i class='bx bx-grid-alt'></i>
-                <span class="links_name">Dashboard</span>
-              </a>
-            </li>
-           
-            <li>
-              <a href="/producerlist.html">
-                <i class='bx bx-box'></i>
-                <span class="links_name">Producer List</span>
-              </a>
-            </li>
-            <li>
-              <a href="/order-list.html">
-                <i class='bx bx-list-ul'></i>
-                <span class="links_name">Order list</span>
-              </a>
-            </li>
-      
-            <li>
-              <a href="/item-list.html">
-                <i class='bx bx-coin-stack'></i>
-                <span class="links_name">Item List</span>
-              </a>
-            </li>
-            <li>
-              <a href="/employee-list.html">
-                <i class='bx bx-book-alt'></i>
-                <span class="links_name">Employee list</span>
-              </a>
-            </li>
-      
-      
-            <li>
-              <a href="/my-profile.html">
-                <i class='bx bx-cog'></i>
-                <span class="links_name">My profile</span>
-              </a>
-            </li>
-      
-            <li>
-              <a href="add.html">
-                <i class='bx bx-cog'></i>
-                <span class="links_name">Add </span>
-              </a>
-            </li>
-            <li class="log_out">
-              <a href="../super_link/super.html">
-                <i class='bx bx-log-out'></i>
-                <span class="links_name">Log out</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <section class="home-section">
-          <nav>
-            <div class="sidebar-button">
-              <i class='bx bx-menu sidebarBtn'></i>
-              <span class="dashboard">Dashboard</span>
-            </div>
-            <div class="search-box">
-              <input type="text" placeholder="Search..."/>
-              <i class='bx bx-search'></i>
-            </div>
-            <div class="profile-details">
-              <img src="images/image.jpg" alt=""/>
-              <span class="admin_name">super link</span>
-             
-            </div>
-          </nav>
+        
+
       
           <div class="home-content">
       
@@ -199,7 +124,7 @@ const Add =()=>{
       
               
             </div>
-        </section>
+        
       
         
       
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,13 +1,172 @@
-
+import { NavLink } from 'react-router-dom';
+import "../Assets/styles/global.css"
+import "../Assets/styles/SideNavBar.css"
 const Index = () => {
-    return (
-     
-      <section className='section'>
-  
-        <h1>home</h1>
+  return (
+
+    <body>
+
+      <section class="home-section">
         
+
+        <div class="home-content">
+          <div class="overview-boxes">
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Order</div>
+                <div class="number">40,876</div>
+                <div class="indicator">
+                  <i class='bx bx-up-arrow-alt'></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class='bx bx-cart-alt cart'></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Sales</div>
+                <div class="number">38,876</div>
+                <div class="indicator">
+                  <i class='bx bx-up-arrow-alt'></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class='bx bxs-cart-add cart two'></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Profit</div>
+                <div class="number">$12,876</div>
+                <div class="indicator">
+                  <i class='bx bx-up-arrow-alt'></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class='bx bx-cart cart three'></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Return</div>
+                <div class="number">11,086</div>
+                <div class="indicator">
+                  <i class='bx bx-down-arrow-alt down'></i>
+                  <span class="text">Down From Today</span>
+                </div>
+              </div>
+              <i class='bx bxs-cart-download cart four'></i>
+            </div>
+          </div>
+
+          <div class="sales-boxes">
+            <div class="recent-sales box">
+              <div class="title">Recent Sales</div>
+              <div class="sales-details">
+                <ul class="details">
+                  <li class="topic">Date</li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink>   </li>
+                  <li> <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>02 Jan 2021</NavLink></li>
+                </ul>
+                <ul class="details">
+                  <li class="topic">Customer</li>
+                  <li> <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Alex Doe</NavLink></li>
+                </ul>
+                <ul class="details">
+                  <li class="topic">Sales</li>
+                  <li> <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Delivered</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Pending</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Returned</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Delivered</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Pending</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Returned</NavLink></li>
+                  <li> <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Delivered</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Pending</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>Delivered</NavLink></li>
+                </ul>
+                <ul class="details">
+                  <li class="topic">Total</li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                  <li><NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>$204.98</NavLink></li>
+                </ul>
+              </div>
+              <div class="button">
+               
+                <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>See All</NavLink>
+              </div>
+            </div>
+            <div class="top-sales box">
+              <div class="title">Top Seling Product</div>
+              <ul class="top-sales-details">
+                <li>
+                  
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}><span class="product">Vuitton Sunglasses</span></NavLink>
+                  <span class="price">$1107</span>
+                </li>
+                <li>
+                 
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}> <span class="product">Hourglass Jeans </span></NavLink>
+                  <span class="price">$1567</span>
+                </li>
+                <li>
+                  
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}> <span class="product">Nike Sport Shoe</span></NavLink>
+                  <span class="price">$1234</span>
+                </li>
+                <li>
+                  
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}>  <span class="product">Hermes Silk Scarves.</span></NavLink>
+                  <span class="price">$2312</span>
+                </li>
+                <li>
+                
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}><span class="product">Succi Ladies Bag</span></NavLink>
+                  <span class="price">$1456</span>
+                </li>
+                <li>
+                
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}><span class="product">Gucci Womens's Bags</span></NavLink>
+                  <span class="price">$2345</span>
+                </li>
+                <li>
+                
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}><span class="product">Addidas Running Shoe</span></NavLink>
+                  <span class="price">$2345</span>
+                </li>
+                <li>
+                  
+                  <NavLink to='/' className={({ isActive }) => (isActive ? 'name' : 'name')}><span class="product">Bilack Wear's Shirt</span></NavLink>
+                  <span class="price">$1245</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
-      
-    );
-  };
-  export default Index;
+
+
+    </body>
+
+  );
+};
+export default Index;
