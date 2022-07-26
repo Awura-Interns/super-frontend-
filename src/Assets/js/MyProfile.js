c911a6ae45cf18189915f9b585b8db7fe52493de
Abigiya Terefe <abigiya1122@gmail.com>
2022-07-26 00:51:17 -0500

    modified myprofile page

---
+++ src/Assets/js/MyProfile.js
@@ -0,0 +1,32 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else
+    sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.onclick = () => {
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target');
+      if (name === target) {
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
