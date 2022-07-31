720275d254b4435125967e2b9775bb8492902b8e
Abigiya Terefe <abigiya1122@gmail.com>
2022-07-31 17:08:25 -0500

    added employee data to render it on the employelist page

---
+++ src/Helpers/EmployeeData.js
@@ -0,0 +1,77 @@
+const employee = [
+    {
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    },
+    {
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    },{
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    },{
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    },{
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    },
+    {
+        "id": "http://127.0.0.1:8000/staff/employee/1/",
+        "user": {
+            "first_name": "abigiya",
+            "last_name": "terefe",
+            "email": "abigiya1122@gmail.com",
+            "phone": "+251931213963",
+            "user_type": "employee"
+        },
+        "profile_picture": "http://127.0.0.1:8000/media/accounts/employee/profile_pictures/mom.png",
+        "birthdate": "2001-04-04",
+        "identification_card": "http://127.0.0.1:8000/media/accounts/employee/id/pngwing.com_4.png"
+    }
+]
