8a45b7c2eaa89eff2006b327b3f86c9059c59263
Abigiya Terefe <abigiya1122@gmail.com>
2022-08-14 15:44:56 -0500

    modified the cart page by adding a table

--- src/Pages/Carts.js
+++ src/Pages/Carts.js
@@ -1,33 +1,57 @@
 import "../Assets/styles/Carts.css"
 import { Link } from 'react-router-dom';
 import carts from '../Helpers/CartData';
+import * as ReactBootStrap from 'react-bootstrap'
 const Carts = () => {
   return (
     <>
-    <h1>Carts list</h1>
-    <section className='section carts'>
-      
-      
+      <h1>Carts list</h1>
+      <section className='section carts'>
 
-    
-      <div className='cart'>
-        {carts.map((cart) => {
-          return (
-            <article className="lemon"  key={cart.id}>
-              <h5>{cart.user}</h5>
-              {/* <h5>{cart.order_date}</h5>
-              <h5>{cart.delivery_man}</h5>
-              <h5>{cart.closed}</h5>
-              <h5>{cart.payed}</h5>
-              <h5>{cart.closed_time}</h5> */}
-              <Link to={`/carts/${cart.id}`}>more info</Link>
-            </article>
-          );
-        })}
-      </div>
-      
-      
-    </section>
+
+
+
+        <div className='cart'>
+          
+              <article   >
+
+
+
+                <div >
+                  <ReactBootStrap.Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>id</th>
+                        <th>user id</th>
+                        <th>order date</th>
+                        <th>closed time</th>
+                        <th>More</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {carts.map((cart) =>{
+                      return(
+                        
+                      <tr key={cart.id}>
+                        <td>{cart.id}</td>
+                        <td>{cart.user}</td>
+                        <td>{cart.order_date}</td>
+                        <td>{cart.closed_time}</td>
+                        <td><Link to={`/carts/${cart.id}`}>more info</Link></td>
+                      </tr>
+                      )
+                    })}
+
+
+                    </tbody>
+                  </ReactBootStrap.Table>
+                </div>
+              </article>
+           
+        </div>
+
+
+      </section>
     </>
   );
 };
