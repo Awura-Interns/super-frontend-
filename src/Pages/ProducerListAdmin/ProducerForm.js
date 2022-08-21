8fe1305bdccaba8c4d739512a235626f40282b66
Abigiya Terefe <abigiya1122@gmail.com>
2022-08-20 21:51:41 -0500

    setup api feacher

--- src/Pages/ProducerListAdmin/ProducerForm.js
+++ src/Pages/ProducerListAdmin/ProducerForm.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import "../../Assets/styles/ProducerList.css"
+import axios from "axios";
+import { useEffect, useState } from 'react'
  const ProducerForm = () => {
+  const [supplier, setSupplier] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    // fetch data from the localhost and save it to the state
+    useEffect(() => {
+        setLoading(true)
+        axios.get('http://127.0.0.1:8000/staff/supplier/')
+            .then(res => {
+                console.log(res.data)
+                setSupplier(res.data)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setError(true)
+            })
+    }, [])
   return (
     <section>
 
