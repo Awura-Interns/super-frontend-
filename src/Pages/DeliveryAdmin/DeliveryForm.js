ba23c03b85abcd4a664c8acb694a8fe4d139587b
Abigiya Terefe <abigiya1122@gmail.com>
2022-08-23 21:17:18 -0500

    able to register delivery

--- src/Pages/DeliveryAdmin/DeliveryForm.js
+++ src/Pages/DeliveryAdmin/DeliveryForm.js
@@ -1,60 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+
+import StaffForm from '../../Components/StaffForm';
 import "../../Assets/styles/button.css"
  const DeliveryForm = () => {
+    const url  = 'http://127.0.0.1:8000/staff/delivery/';
+  
+  
+    const handleSubmit = (event)=>{
+      event.preventDefault();
+      const data = {
+          "user": {
+              "first_name": event.target.first_name.value,
+              "last_name": event.target.last_name.value,
+              "email": event.target.email.value,
+              "password": event.target.password.value,
+              "confirm_password": event.target.confirm_password.value,
+              "phone": event.target.phone.value
+          },
+          "profile_picture": event.target.profile_picture.files[0],
+          "birthdate": event.target.birthdate.value,
+          "identification_card": event.target.identification_card.files[0]
+      }
+      axios.request({
+          method: 'post',
+          headers: {
+              "Content-Type": "multipart/form-data",
+          },
+          url,
+          data
+      }).then(res => {
+          console.log(res)
+      })
+    }
   return (
-    <section>
+    <>
+        <StaffForm handleSubmit={handleSubmit}/>
 
-                    <div class="Employewrapper">
-                        <div class="title">
-                            Delivery registration
-                        </div>
-                        <form class="form">
-                            <div class="inputfield">
-                                <label>First Name</label>
-                                <input type="text" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Last Name</label>
-                                <input type="text" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Email</label>
-                                <input type="email" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Password</label>
-                                <input type="password" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Confirm password</label>
-                                <input type="password" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Phone</label>
-                                <input type="tel" class="input" />
-                            </div>
-
-                            <div class="inputfield">
-                                <label for="file">Profile Picture</label>
-                                <input type="file" id="file" accept="image/*" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label>Birthdate</label>
-                                <input type="date" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <label for="file">Id Card</label>
-                                <input type="file" id="file" accept="image/*" class="input" />
-                            </div>
-                            <div class="inputfield">
-                                <input type="submit" class="btn" />
-                            </div>
-                            <Link to='/Delivery' className='btn'>Back Delivery</Link> 
-                        </form>
-                    </div>
-                </section>
+    </>
   )
 }
 export default DeliveryForm
