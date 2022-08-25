42883e016f10af986269658c45b4b539bace5a92
Abigiya Terefe <abigiya1122@gmail.com>
2022-08-25 13:26:01 -0500

    added a select in the product form where the option are supplier full name, it was feached from the supplier api

--- src/Pages/ProducerListAdmin/ProducerForm.js
+++ src/Pages/ProducerListAdmin/ProducerForm.js
@@ -3,76 +3,101 @@ import { Link } from 'react-router-dom';
 import "../../Assets/styles/ProducerList.css"
 import axios from "axios";
 import { useEffect, useState } from 'react'
- const ProducerForm = () => {
+const ProducerForm = () => {
   const [supplier, setSupplier] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
-    // fetch data from the localhost and save it to the state
-    useEffect(() => {
-        setLoading(true)
-        axios.get('http://127.0.0.1:8000/staff/supplier/')
-            .then(res => {
-                console.log(res.data)
-                setSupplier(res.data)
-                setLoading(false)
-            })
-            .catch(err => {
-                console.log(err)
-                setError(true)
-            })
-    }, [])
+  // fetch data from the localhost and save it to the state
+  useEffect(() => {
+    setLoading(true)
+    axios.get('http://127.0.0.1:8000/staff/supplier/')
+      .then(res => {
+        console.log(res.data)
+        setSupplier(res.data)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log(err)
+        setError(true)
+      })
+  }, [])
   return (
     <section>
 
 
-      <div class="Employewrapper">
-        <div class="title">
-        Producer registration
+      <div className="Employewrapper">
+        <div className="title">
+          Producer registration
         </div>
-        <form class="form">
-        <div class="inputfield">
+        <form className="form">
+          <div className="inputfield">
             <label for="file">Product Picture</label>
-            <input type="file" id="file" accept="image/*" class="input" />
+            <input type="file" id="file" accept="image/*" className="input" />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label for="file">agreement Picture</label>
-            <input type="file" id="file" accept="image/*" class="input" />
+            <input type="file" id="file" accept="image/*" className="input" />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label>product_name </label>
-            <input type="text" class="input" />
+            <input type="text" className="input" />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label>product type</label>
-            <input type="text" class="input" />
+            <input type="text" className="input" />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label>description</label>
-            <input type="textarea" class="input" maxlength="100"  placeholder='100 characters'/>
+            <input type="textarea" className="input" maxLength="100" placeholder='100 characters' />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label>Amount</label>
-            <input type="number" class="input" placeholder='IN kilogram' />
+            <input type="number" className="input" placeholder='IN kilogram' />
           </div>
-          <div class="inputfield">
+          <div className="inputfield">
             <label>price</label>
-            <input type="number" class="input" />
+            <input type="number" className="input" />
           </div>
 
-          
-          <div class="inputfield">
+
+          <div className="inputfield">
             <label>Discount</label>
-            <input type="number" class="input"  maxlength="1" placeholder='in percentage'/>
+            <input type="number" className="input" maxLength="1" placeholder='in percentage' />
           </div>
-          {/* <div class="inputfield">
-            <label for="file">Id Card</label>
-            <input type="file" id="file" accept="image/*" class="input" />
-          </div>*/}
-          <div class="inputfield">
+          <div className="inputfield">
+            <label for="file">Producer</label>
+
+
+            <select className="input">
+            {supplier.map((supplier) => {
+             
+              return (
+               
+
+                  <option value={supplier.id}>{supplier.user.first_name} {supplier.user.last_name}</option>
+
+              )
+            })}
+                </select>
+
+            {/* <select className="input">
+
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+              <option>hellow</option>
+
+            </select> */}
+          </div>
+          <div className="inputfield">
             <input type="submit" className="btn" />
-        </div> 
-        <Link to='/Producer' className='btn'>Back Producers</Link> 
+          </div>
+          <Link to='/Producer' className='btn'>Back Producers</Link>
         </form>
       </div>
     </section>
