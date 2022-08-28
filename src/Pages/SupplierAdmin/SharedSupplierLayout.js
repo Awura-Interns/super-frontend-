5084fc90aee8587ee876b5b48c97673c9b22f9ed
Abigiya Terefe <abigiya1122@gmail.com>
2022-08-28 00:41:36 -0500

    remove unnecessary import

--- src/Pages/SupplierAdmin/SharedSupplierLayout.js
+++ src/Pages/SupplierAdmin/SharedSupplierLayout.js
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import React from 'react'
-import SideNavBar from '../../Components/SideNavBar'
-import  Top from "../../Components/Top"
+
+
 
 const Supplier = () => {
   return (
