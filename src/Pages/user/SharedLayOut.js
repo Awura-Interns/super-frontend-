8c93af55b178fcbb19fab6a622462d6c0255d055
Abigiya Terefe <abigiya1122@gmail.com>
2022-09-06 05:36:06 -0500

    Merged

--- src/Pages/user/SharedLayOut.js
+++ src/Pages/user/SharedLayOut.js
@@ -1,5 +1,6 @@
 import {Outlet } from 'react-router-dom';
 import Footer from "../../Components/user/Footer"
+import "../../index.css"
 const Home = () => {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../src/Assets/admin/styles/index.css';
+// import '../src/Assets/admin/styles/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
