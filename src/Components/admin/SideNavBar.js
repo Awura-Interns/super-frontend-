e494fbea58544785f7dfbcd50f0579be105bfdc1
Abigiya Terefe <abigiya1122@gmail.com>
2022-09-10 04:52:42 -0500

    Fixed admin page css problem

--- src/Components/admin/SideNavBar.js
+++ src/Components/admin/SideNavBar.js
@@ -3,36 +3,48 @@ import "../../Assets/admin/styles/global.css"
 import "../../Assets/admin/styles/SideNavBar.css"
 import AuthContext from '../../Auth/AuthContect';
 import { useContext } from 'react';
+
+
+const FontSizeTitle={
+    fontSize: "3rem",
+    fontWeight: "bolder"
+
+}
+const FontSizeBtn={
+    fontSize: "2rem",
+    textTransform: "capitalize"
+
+}
 export default function SideNavBar() {
     const { logoutUser } = useContext(AuthContext);
     return (
         <>
-        <div class="sidebar">
+        <div class="sidebar" >
                 <div class="logo-details">
                     <i class='bx bxl-c-plus-plus'></i>
-                    <span class="logo_name">super link</span>
+                    <span class="logo_name" style={FontSizeTitle}>super link</span>
                 </div>
                 <ul class="nav-links">
-                    <li>
-                        <NavLink to='/admin-page' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class='bx bxs-home-smile'></i><span class="links_name">home</span></NavLink>
+                    <li >
+                        <NavLink to='/admin-page' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class='bx bxs-home-smile'></i><span class="links_name" style={FontSizeBtn}>home</span></NavLink>
                     </li>
                     <li>
-                        <NavLink to='/admin-page/product' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class='bx bx-box'></i><span class="links_name">Product List</span></NavLink>
+                        <NavLink to='/admin-page/product' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class='bx bx-box'></i><span class="links_name" style={FontSizeBtn}>Product List</span></NavLink>
                     </li>
                     <li>
-                        <NavLink to='/admin-page/carts-admin' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class='bx bx-list-ul'></i><span class="links_name">Carts list</span></NavLink>
+                        <NavLink to='/admin-page/carts-admin' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class='bx bx-list-ul'></i><span class="links_name" style={FontSizeBtn}>Carts list</span></NavLink>
                     </li>
                     <li>
-                        <NavLink to='/admin-page/employeeList' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class='bx bx-book-alt'></i><span class="links_name">Employee list</span></NavLink>
+                        <NavLink to='/admin-page/employeeList' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class='bx bx-book-alt'></i><span class="links_name" style={FontSizeBtn}>Employee list</span></NavLink>
                     </li>
                     <li>
-                        <NavLink to='/admin-page/delivery' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class="fa-solid fa-truck-pickup"></i><span class="links_name">Delivery </span></NavLink>
+                        <NavLink to='/admin-page/delivery' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class="fa-solid fa-truck-pickup"></i><span class="links_name" style={FontSizeBtn}>Delivery </span></NavLink>
                     </li>
                     <li>
-                        <NavLink to='/admin-page/supplier' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i class="fa-solid fa-boxes-stacked"></i><span class="links_name">Supplier</span></NavLink>
+                        <NavLink to='/admin-page/supplier' className={({ isActive }) => (isActive ? 'links_name' : 'links_name')}><i style={FontSizeBtn} class="fa-solid fa-boxes-stacked"></i><span class="links_name" style={FontSizeBtn}>Supplier</span></NavLink>
                     </li>
                     <li>
-                        <button className="links_name" type="button" onClick={logoutUser}><i class='bx bx-log-in-circle'></i><span class="links_name">Log out</span> </button>
+                        <button className="links_name" type="button" onClick={logoutUser}><i style={FontSizeBtn} class='bx bx-log-in-circle'></i><span class="links_name" style={FontSizeBtn}>Log out</span> </button>
                     </li>
                 </ul>
             </div>
--- src/Components/admin/Top.js
+++ src/Components/admin/Top.js
@@ -4,6 +4,19 @@ import "../../Assets/admin/styles/SideNavBar.css"
 import axios from "axios";
 import logo from "../../Assets/admin/img/s.png"
 import { useEffect, useState } from 'react'
+
+const FontSizeTitle={
+  fontSize: "3rem",
+  fontWeight: "bolder"
+
+}
+const email={
+  fontWeight:"bolder",
+  fontSize:"2rem",
+  fontFamily:"system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+ 
+  
+}
 export default function SideNavBar() {
     const [user, setUser] = useState([])
     const [loading, setLoading] = useState(true)
@@ -27,11 +40,11 @@ export default function SideNavBar() {
 <nav>
     <div class="sidebar-button">
         {/* <i class='bx bx-menu sidebarBtn'></i> */}
-        <span class="dashboard">Dashboard</span>
+        <span class="dashboard" style={FontSizeTitle}>Dashboard</span>
     </div>
     <div class="profile-details">
         <img src={logo} alt="" />
-        <span class="admin_name">{user.email}</span>
+        <span class="admin_name" style={email}>{user.email}</span>
     </div>
 </nav>
 </div>
