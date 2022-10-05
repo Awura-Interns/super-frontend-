88b6ef2d68bde135db352136524053a0e2dc780b
Abigiya Terefe <abigiya1122@gmail.com>
2022-10-05 09:20:28 -0500

    admin page employee is responsive

--- src/Pages/admin/StaffEmployeeAdmin/EmployeeList.js
+++ src/Pages/admin/StaffEmployeeAdmin/EmployeeList.js
@@ -3,48 +3,48 @@ import "../../../Assets/admin/styles/actionBtn.css"
 import axios from "axios";
 import * as ReactBootStrap from 'react-bootstrap'
 import { useEffect, useState } from 'react'
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
 
 const positionSection = {
     marginTop: "8rem",
-  }
-  const positionTitle = {
+}
+const positionTitle = {
     marginLeft: "15rem",
     fontSize: "3rem",
     fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     fontWeight: "bolder",
     color: "rgb(2, 2, 56)"
-  }
-  const RegBtn = {
+}
+const RegBtn = {
     fontSize: "2rem"
-  }
-  const tableHeading = {
+}
+const tableHeading = {
     fontSize: "1.5rem"
-  }
-  const tableContent = {
+}
+const tableContent = {
     fontSize: "1.5rem",
     fontWeight: "600",
-  }
-  const actionBox = {
+}
+const actionBox = {
     fontSize: "1rem",
-    padding:"0rem 1rem 0rem 1rem",
+    padding: "0rem 1rem 0rem 1rem",
     backgroundColor: "transparent"
-  
-  }
-  const actionBtn = {
+
+}
+const actionBtn = {
     fontSize: "2rem",
-    padding:"0"
-  }
-  const tableSize={
+    padding: "0"
+}
+const tableSize = {
     width: "140vh",
     height: "60vh"
-  }
+}
 const EmployeeList = () => {
     const [employees, setEmployees] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
-     const listEmployees =()=>{
+    const listEmployees = () => {
         axios.request({
             method: 'get',
             headers: {
@@ -53,12 +53,12 @@ const EmployeeList = () => {
             },
             url: 'https://dev.api.superlink.awuraplc.org/staff/employee/'
         }).then(res => {
-            console.log(res.data)        
+            console.log(res.data)
             setEmployees(res.data)
             setLoading(false)
         })
     }
-    useEffect(() => {  
+    useEffect(() => {
         listEmployees()
     }, [])
     const handleDelete = (id) => {
@@ -69,73 +69,76 @@ const EmployeeList = () => {
                 "Authorization": `Bearer ${JSON.parse(localStorage.getItem("authTokens")).access}`
             },
             url: `https://dev.api.superlink.awuraplc.org/staff/employee/${id}/`
-        }).then(()=>{
+        }).then(() => {
             listEmployees()
-        }).catch(error=>{
+        }).catch(error => {
             alert(error.response.data);
         })
     }
-    if(loading){
+    if (loading) {
         return (
-        <div style={{width:"100vw", height:"100vh", backgroundColor:"rgba(0,0,0,0.6)", display:"flex", alignItems:"center", justifyContent:"center"}}>         
-            <Spinner animation="border" style={{color:"white"}} />
-        </div>
+            <div style={{ width: "100vw", height: "100vh", backgroundColor: "rgba(0,0,0,0.6)", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <Spinner animation="border" style={{ color: "white" }} />
+            </div>
         )
     }
     return (
-        
-            <body className="Body">
-                <section class="employee_listing_whole" style={positionSection}>
-                    <h1 style={positionTitle}>Employee listing<NavLink to='/admin-page/employeeList/employeeForm' className={({ isActive }) => (isActive ? 'addEmployee' : 'addEmployee')} ><i class='bx bxs-add-to-queue' title="add"></i> </NavLink> </h1>
 
-                    <div class="employee_listing" style={tableSize}>
-                       
-                        <ReactBootStrap.Table striped bordered hover>
-                            <thead>
-                                <tr>
-                                    <th style={tableHeading}>No.</th>
-                                    <th style={tableHeading}>photo</th>
-                                    <th style={tableHeading}>name</th>
-                                    <th style={tableHeading}>email</th>
-                                    <th style={tableHeading}>phone number</th>
-                                    <th style={tableHeading}>birthdate</th>
-                                    <th style={tableHeading}>identification card</th>
-                                    <th style={tableHeading}>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {employees.map((employee,index) => {
-                                    return (
-                                        <tr key={index + 1}>
+        <body className="Body">
+            <section class="employee_listing_whole" style={positionSection}>
+                <h1 style={positionTitle}>Employee listing<NavLink to='/admin-page/employeeList/employeeForm' className={({ isActive }) => (isActive ? 'addEmployee' : 'addEmployee')} ><i class='bx bxs-add-to-queue' title="add"></i> </NavLink> </h1>
+
+                <div class="employee_listing" style={tableSize}>
+
+                    <ReactBootStrap.Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th style={tableHeading}>No.</th>
+                                <th style={tableHeading}>photo</th>
+                                <th style={tableHeading}>name</th>
+                                <th style={tableHeading}>email</th>
+                                <th style={tableHeading}>phone number</th>
+                                <th style={tableHeading}>birthdate</th>
+                                <th style={tableHeading}>identification card</th>
+                                <th style={tableHeading}>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {employees.map((employee, index) => {
+                                return (
+                                    <tr key={index + 1}>
                                         <td style={tableContent}>{index + 1}</td>
-                                            <td style={tableContent}> <img className="image_profile_picture" src={employee.profile_picture} alt="Mark Zuckerberg"
-                                            /> </td>
-                                            <td style={tableContent}>{employee.user.first_name} {employee.user.last_name}</td>
-                                            <td style={tableContent}>{employee.user.email}</td>
-                                            <td style={tableContent}>{employee.user.phone}</td>
-                                            <td style={tableContent}>{employee.birthdate}</td>
-                                            <td style={tableContent}><img src={employee.identification_card} alt={employee.user.first_name}
-                                                className="image_profile_picture" /></td>
-                                            <td style={tableContent}>
-                                                <div className="action_btn">
-                                                    <div className='btn' style={actionBox}>
-                                                    <button className="edite"><Link  to={`/admin-page/employeeList/employeeEdit/${employee.pk}`} style={actionBtn}><i className='bx bx-pencil'></i></Link></button>  
-                                                    </div>
-                                                    <div className='btn' style={actionBox}>
-                                                        <button onClick={() => handleDelete(employee.pk)} className="delete" title="delete" style={actionBtn}>
-                                                            <i className='bx bxs-trash-alt'></i>
-                                                        </button>
-                                                    </div>
-                                                </div>  </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </ReactBootStrap.Table>
-                    </div>
-                </section>
-            </body>
-        
+
+                                        <td style={tableContent}> <a href={employee.profile_picture} target="_blank"><img className='image_profile_picture' src={employee.profile_picture} alt={employee.user.first_name} /></a> </td>
+
+
+
+                                        <td style={tableContent}>{employee.user.first_name} {employee.user.last_name}</td>
+                                        <td style={tableContent}>{employee.user.email}</td>
+                                        <td style={tableContent}>{employee.user.phone}</td>
+                                        <td style={tableContent}>{employee.birthdate}</td>
+                                        
+                                             <td style={tableContent}> <a href={employee.identification_card} target="_blank"><img className='image_profile_picture' src={employee.identification_card} alt={employee.user.first_name} /></a> </td>
+                                        <td style={tableContent}>
+                                            <div className="action_btn">
+                                                <div className='btn' style={actionBox}>
+                                                    <button className="edite"><Link to={`/admin-page/employeeList/employeeEdit/${employee.pk}`} style={actionBtn}><i className='bx bx-pencil'></i></Link></button>
+                                                </div>
+                                                <div className='btn' style={actionBox}>
+                                                    <button onClick={() => handleDelete(employee.pk)} className="delete" title="delete" style={actionBtn}>
+                                                        <i className='bx bxs-trash-alt'></i>
+                                                    </button>
+                                                </div>
+                                            </div>  </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </ReactBootStrap.Table>
+                </div>
+            </section>
+        </body>
+
     )
 }
 export default EmployeeList;
